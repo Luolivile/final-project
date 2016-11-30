cc8d080842b557e5abf827f84406f3522abc77b5
Luolivile <Luolivile@Lucilas-MacBook.local>
2016-11-30 16:53:51 -0500

    changes

--- js/main.js
+++ js/main.js
@@ -2,6 +2,29 @@
 //get all thumbs
 var thumbs =  document.getElementsByClassName('thumb');
 
+function deselect () {
+  document.body.querySelector('.selected').classList.remove('selected');
+  document.body.classList.remove('thumbSelected');
+  document.querySelector('.rowSelected').classList.remove('rowSelected');
+}
+
+function goToThumb (direction) {
+  for (var i = 0; i < thumbs.length; i++) {
+    if (thumbs[i].classList.contains('selected')) {
+      thumbs[i].classList.remove('selected');
+      document.querySelector('.rowSelected').classList.remove('rowSelected');
+
+      var next = i + direction;
+      next = (next >= thumbs.length ? 0 : next); // too high?
+      next = (next < 0 ? thumbs.length - 1 : next); // too low?
+
+      thumbs[next].classList.add('selected');
+      thumbs[next].parentNode.classList.add('rowSelected');
+      break;
+    }
+  }
+}
+
 //changes background color on hover
 for (i=0; i<thumbs.length; i++) {
   thumbs[i].addEventListener('mouseover', function (evt) {
@@ -13,59 +36,26 @@ for (i=0; i<thumbs.length; i++) {
     //remove class selected
     //clicked on selected thumb
     if (this.classList.contains('selected')) {
-      this.classList.remove('selected');
-      document.body.classList.remove('thumbSelected');
-      this.querySelector('.rowSelected').classList.remove('rowSelected');
+      deselect();
 
     } else {
-        var selected = document.querySelector('.selected')
-        if (selected !== null) {
-          selected.classList.remove('selected');
-        }
-        this.classList.add('selected');
-        this.parentNode.classList.add('rowSelected');
-        document.body.classList.add('thumbSelected');
+      var selected = document.querySelector('.selected')
+      if (selected !== null) {
+        selected.classList.remove('selected');
+      }
+      this.classList.add('selected');
+      this.parentNode.classList.add('rowSelected');
+      document.body.classList.add('thumbSelected');
     }
   })
 }
 
-    document.querySelector('#close').addEventListener('click', function() {
-        this.classList.contains('thumbSelected');
-        this.classList.remove('thumbSelected');
-        document.body.classList.remove('thumbSelected');
-        document.querySelector('.thumb').classList.remove('selected');
-    })
+document.querySelector('#close').addEventListener('click', deselect);
 
-   document.querySelector('#right').addEventListener('click', function(){
-      for (i=0; i<thumbs.length; i++) {
-        if (thumbs[i].classList.contains('selected')) {
-          thumbs[i].classList.remove('selected');
-          this.querySelector('.rowSelected').classList.remove('rowSelected');
-          if (i+1 >= thumbs.length) {
-            var next = 0
-          } else {
-            var next = i+1
-          }
-          thumbs[next].classList.add('selected');
-          this.parentNode.classList.add('rowSelected');
-          break;
-        }
-      }
-   })
+document.querySelector('#right').addEventListener('click', function() {
+  goToThumb(1)
+});
 
-   document.querySelector('#left').addEventListener('click', function (){
-     for (i = thumbs.length - 1; i >= 0; i--) {
-       if (thumbs[i].classList.contains('selected')) {
-         thumbs[i].classList.remove('selected');
-         this.querySelector('.rowSelected').classList.remove('rowSelected');
-         if (i-1 <= 0) {
-           var previous = thumbs.length -1
-         } else {
-           var previous = i-1
-         }
-         thumbs[previous].classList.add('selected');
-         this.parentNode.classList.add('rowSelected');
-         break;
-       }
-     }
-   })
+document.querySelector('#left').addEventListener('click', function() {
+  goToThumb(-1)
+});
